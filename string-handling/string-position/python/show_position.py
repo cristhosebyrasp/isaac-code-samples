c4ab1a211be51d953326c3542ad0a8f71c3b75cf
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2021-08-05 10:46:00 +0100

    Update show_position.py

--- string-handling/string-position/python/show_position.py
+++ string-handling/string-position/python/show_position.py
@@ -2,7 +2,7 @@
 # Usage licensed under the Open Government Licence v3.0
 
 def show_position():
-    """Demonstrates how to find a character within a string"""
+    # Demonstrates how to find a character within a string
     subject = "Computer Science"
     position = subject.find("m")
     print(f"Found at: {position}")
