98eff8c6a505d644b81a6cfaf53fc095de8f4625
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2021-08-05 10:11:49 +0100

    Update greetings.py

--- string-handling/string-concatenation/python/greetings.py
+++ string-handling/string-concatenation/python/greetings.py
@@ -2,7 +2,7 @@
 # Usage licensed under the Open Government Licence v3.0
 
 def greetings():
-    """Demonstration of string concatenation"""
+    # Demonstration of string concatenation
     first_name = input("Please enter your name ")
     print("Hello " + first_name)
     last_name = input("Please enter your surname ")
