d901062a5931790e1a7375ac71e971cfeeed27d0
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2021-08-05 10:31:55 +0100

    Update show_password_length.py

--- string-handling/string-characteristics/python/show_password_length.py
+++ string-handling/string-characteristics/python/show_password_length.py
@@ -2,7 +2,7 @@
 # Usage licensed under the Open Government Licence v3.0
 
 def show_password_length():
-    """Prompts for a password and displays length"""
+    # Prompts for a password and displays length
     password = input("Please enter a password ")
     print(len(password))
 
