2ee65fbd0b686bbe9c1e96bd7da07ef9969e180a
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2021-08-07 16:34:18 +0100

    Update read-a-file.py

--- file-handling/read-a-file.py
+++ file-handling/read-a-file.py
@@ -1,1 +1,12 @@
+"""
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and run on your machine.
+
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the Python IDE of your choice (PyCharm, for example)
+3. Run the program
+"""
+
 file_data = open("example.txt", "r")
