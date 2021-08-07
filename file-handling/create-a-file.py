d6f60b10d6be2249709b1a9e51d461c62e90b69f
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2021-08-07 16:26:40 +0100

    Update create-a-file.py

--- file-handling/create-a-file.py
+++ file-handling/create-a-file.py
@@ -1,2 +1,13 @@
-newfile = open("myfile.txt", "x")
+"""
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and run on your machine.
+
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the Python IDE of your choice (PyCharm, for example)
+3. Run the program
+"""
+
+newfile = open("myfile.txt", "x") 
 newfile.close()
